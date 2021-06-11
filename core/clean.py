36cf230d75aa24f437d1b1200262c97ef34bfe51
softmilkpc <70849324+softmilkpc@users.noreply.github.com>
2021-06-11 17:00:14 +0700

    Create clean.py

---
+++ core/clean.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+from configs import Config
+
+async def delete_trash(file):
+    try:
+        os.remove(file)
+    except Exception as e:
+        print(e)
+
+async def delete_all():
+    try:
+        root = Config.DOWN_PATH + "/WatermarkAdder/"
+        shutil.rmtree(root)
+    except Exception as e:
+        print(e)
